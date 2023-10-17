bffc3217460370d57bdbf233a4a31ded09c9ff93
zummrs <100191377+zummrs@users.noreply.github.com>
2023-10-16 19:33:49 -0700

    Add files via upload

---
+++ 491-FSAH/scripts/main.js
@@ -0,0 +1,7 @@
+function openSidePanel() {
+    document.getElementById("sidePanel").style.left = "0";
+}
+
+function closeSidePanel() {
+    document.getElementById("sidePanel").style.left = "-250px";
+}
