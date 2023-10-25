8305f3d0ad68e6b8fa12ee0b05eb195d3adfd9b9
Axelr25 <114271888+Axelr25@users.noreply.github.com>
2023-10-24 18:31:58 -0700

    Update main.js

--- 491-FSAH/scripts/main.js
+++ 491-FSAH/scripts/main.js
@@ -5,3 +5,17 @@ function openSidePanel() {
 function closeSidePanel() {
     document.getElementById("sidePanel").style.left = "-250px";
 }
+
+
+document.getElementById('searchInput').addEventListener('input', function() {
+    let searchTerm = this.value.toLowerCase();
+    let buttons = document.querySelectorAll('.bubble-button, .Newbubble-button');
+
+    buttons.forEach(button => {
+        if (button.getAttribute('data-name').toLowerCase().includes(searchTerm)) {
+            button.style.display = 'block';
+        } else {
+            button.style.display = 'none';
+        }
+    });
+});
