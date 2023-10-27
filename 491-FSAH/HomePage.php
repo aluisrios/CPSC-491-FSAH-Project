1e7b6d6f3ba88ce673088fbb74db3d9777f3dcad
aluisrios <112264349+aluisrios@users.noreply.github.com>
2023-10-27 12:49:55 -0700

    Add files via upload

---
+++ 491-FSAH/HomePage.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <link rel="stylesheet" href="CSS/Home.css">
+</head>
+
+<body>
+
+    <div class="Banner">
+
+        <div class="navbar">
+            <img src="LOGO.png" class="logo">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Apply</a></li>
+                <li><a href="#">Support</a></li>
+            </ul>
+        </div>
+
+    <div class="content">
+        <h1> Make applying for Financial Easy </h1>
+        <p> FSAH is here to get all the Financial aid information in one place,
+        <br>we can help with any question you make have, or how you could apply. </p>
+       
+        <div class="button">
+            <button type="Apply"><a href='SignUp.php' ><span></span>APPLY </button>
+            <button tyoe="button"><span></span> SUPPORT </button>
+        </div>
+
+    </div>
+
+
+    </div>
+
+
+</body>
+</html>
