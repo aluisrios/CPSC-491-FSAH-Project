25ee8cbc06bb2fc74e65db117761e243cf158c5c
Axelr25 <114271888+Axelr25@users.noreply.github.com>
2023-11-13 17:40:49 -0800

    Update index.php

--- 491-FSAH/index.php
+++ 491-FSAH/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FSAH</title>
-    <link rel="stylesheet" href="CSS/styles.css" >
-    <link rel="stylesheet" href="CSS/robot.css">
+    <link rel="stylesheet" href="styles.css" >
+    <link rel="stylesheet" href="robot.css">
 </head>
 <body>
 
@@ -35,8 +35,9 @@
     <div class="side-panel" id="sidePanel">
         <div class="close-button" onclick="closeSidePanel()">×</div>
         <a href="page7.html" class="Panelbubble-button" data-name="Support">Support</a>
-        <a href="page8.php" class="Panelbubble-button" data-name="Bookmark">Bookmark</a>
-        <a href="profile.php" class="Panelbubble-button" data-name="Profile">Profile</a>
+        <a href="page8.html" class="Panelbubble-button" data-name="Bookmark">Bookmark</a>
+        <a href="page9.html" class="Panelbubble-button" data-name="Filter">Filter</a>
+
     </div>
 
     <!-- Robot structure and animations -->
@@ -53,7 +54,69 @@
     <div class="website-prompt">Welcome to FSAH! Here at Financial Student Aid Helper (FSAH), we're all about helping you get through college with as little debt as possible. 
         We've gathered every financial aid option available in your school to make finding and applying for assistance easy and quick—no more waiting weeks for advisor appointments. 
         With FSAH, you're on track to land your dream job after graduation without the heavy burden of student loans!</div>
+        <!-- Next button -->
+        <button id="next-button" style="display: none;">Next</button>
+        <div class="additional-info">
+        Do you need help with anything? Yes or No?
+        <div class="button-container">
+            <button id="yes-button">Yes</button>
+            <button id="no-button">No</button>
+        </div>
 
-    <script src="scripts/main.js"></script>
+    </div>
+
+<!-- This will be hidden initially and only shown when "Yes" is clicked -->
+<div class="help-message" style="display: none;">
+    How can I help you?
+    <div class="help-buttons">
+        <button id="filter">Filter</button>
+        <button id="support">Support</button>
+        <button id="profile">Profile</button>
+        <button id="continue">Continue</button>
+    </div>
+</div>
+
+<!-- Filter Prompt -->
+<div id="filter-prompt" style="display: none;">
+    <h2>Filter Criteria</h2>
+    <form id="filter-form">
+        <!-- Filter by Type of Aid -->
+        <div>
+            <label for="type-of-aid">Type of Aid:</label>
+            <select id="type-of-aid">
+                <option value="grant">Grant</option>
+                <option value="scholarship">Scholarship</option>
+                <option value="loan">Loan</option>
+            </select>
+        </div>
+
+        <!-- Filter by Eligibility -->
+        <div>
+            <label for="eligibility">Eligibility:</label>
+            <select id="eligibility">
+                <option value="undergraduate">Undergraduate</option>
+                <option value="graduate">Graduate</option>
+                <option value="international">International Students</option>
+            </select>
+        </div>
+
+        <!-- Filter by Grant Amount -->
+        <div>
+            <label for="grant-amount">Grant Amount:</label>
+            <select id="grant-amount">
+                <option value="under5000">Under $5000</option>
+                <option value="5000-10000">$5000 - $10000</option>
+                <option value="above10000">Above $10000</option>
+            </select>
+        </div>
+
+        <!-- Submit button -->
+        <button type="submit">Apply Filters</button>
+    </form>
+    <button onclick="closeFilterPrompt()">Close</button>
+</div>
+
+
+    <script src="main.js"></script>
 </body>
 </html>
