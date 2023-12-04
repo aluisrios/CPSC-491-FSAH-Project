6ed3240e4a11f66a4d04d200246b7f47b9d30c2d
axel ramos <Axel>
2023-12-03 16:54:28 -0800

    submission

--- 491-FSAH/index.php
+++ 491-FSAH/index.php
@@ -9,115 +9,83 @@
 </head>
 <body>
 
-    <div class="back">
-            <!-- Container for side panel button and search bar -->
-            <div class="top-container">
-            <!-- Button to open the side panel -->
-            <div class="menu-button" onclick="openSidePanel()" style="cursor: pointer; font-size: 35px; color: orange;">☰</div>
+<div class="top-container">
+    <!-- Side Panel Button -->
+    <div class="menu-button" onclick="openSidePanel()">☰</div>
 
-            <!-- Search Bar -->
-            <div class="search-container">
-                <input type="text" id="searchInput" placeholder="Search..." oninput="searchFunction()">
-            </div>  
+    <!-- Search Bar -->
+    <div class="search-container">
+        <input type="text" id="searchInput" placeholder="Search..." oninput="searchFunction()">
     </div>
 
-    <!-- Bubble buttons container -->
-    <div class="button-container">
-        <a href="page1.html" class="bubble-button" data-name="Cal Grant A">Cal Grant A</a>
-        <a href="page2.html" class="bubble-button" data-name="Cal Grant B">Cal Grant B</a>
-        <a href="page3.html" class="bubble-button" data-name="Pell Grant">Pell Grant</a>
-        <a href="page4.html" class="bubble-button" data-name="FSEOG">FSEOG</a>
-        <a href="page5.html" class="bubble-button" data-name="MCS">MCS</a>
-        <a href="page6.html" class="bubble-button" data-name="SUG">SUG</a>
+    <!-- Home and Sign Up/Log out Buttons -->
+    <div class="top-right-buttons">
+        <button onclick="location.href='HomePage.php'">Home</button>
+        <button onclick="location.href='LoginPage.php'">Log out</button>
     </div>
+</div>
 
-    <!-- Side panel -->
-    <div class="side-panel" id="sidePanel">
-        <div class="close-button" onclick="closeSidePanel()">×</div>
-        <a href="profile.php" class="Panelbubble-button" data-name="profile">Profile</a>
-        <a href="supportPage.html" class="Panelbubble-button" data-name="Support">Support</a>
-        <a href="page8.html" class="Panelbubble-button" data-name="Bookmark">Bookmark</a>
-        <a href="page9.html" class="Panelbubble-button" data-name="Filter">Filter</a>
+<!-- Bubble buttons container -->
+<div class="button-container">
+    <a href="page1.html" class="bubble-button" data-name="Cal Grant A">Cal Grant A</a>
+    <a href="page2.html" class="bubble-button" data-name="Cal Grant B">Cal Grant B</a>
+    <a href="page3.html" class="bubble-button" data-name="Pell Grant">Pell Grant</a>
+    <a href="page4.html" class="bubble-button" data-name="FSEOG">FSEOG</a>
+    <a href="page5.html" class="bubble-button" data-name="MCS">MCS</a>
+    <a href="page6.html" class="bubble-button" data-name="SUG">SUG</a>
+</div>
 
-    </div>
+<!-- Side panel -->
+<div class="side-panel" id="sidePanel">
+    <div class="close-button" onclick="closeSidePanel()">×</div>
+    <a href="profile.php" class="Panelbubble-button" data-name="profile">Profile</a>
+    <a href="supportPage.html" class="Panelbubble-button" data-name="Support">Support</a>
+    <a href="page8.html" class="Panelbubble-button" data-name="Bookmark">Bookmark</a>
+    <a href="page9.html" class="Panelbubble-button" data-name="filter">Filter</a>
+</div>
 
+<!-- Flex container for robot and messages -->
+<div class="robot-and-messages">
     <!-- Robot structure and animations -->
-    <div class="face"></div>
-    <div class="head"></div>
-    <div class="body"></div>
-    <div class="left-hand"></div>
-    <div class="right-hand"></div>
-    <div class="eye1"></div>
-    <div class="eye2"></div>
+    <div class="robot">
+        <div class="face"></div>
+        <div class="head"></div>
+        <div class="body"></div>
+        <div class="left-hand"></div>
+        <div class="right-hand"></div>
+        <div class="eye1"></div>
+        <div class="eye2"></div>
+    </div>
 
-    <!-- Robot introduction and website prompt -->
-    <div class="robot-introduction">Hello, I'm Jerry!</div>
-    <div class="website-prompt">Welcome to FSAH! Here at Financial Student Aid Helper (FSAH), we're all about helping you get through college with as little debt as possible. 
+    <!-- Container for messages -->
+    <div class="messages">
+        <div class="robot-introduction">Hello, I'm Jerry!</div>
+        <div class="website-prompt">Welcome to FSAH! Here at Financial Student Aid Helper (FSAH), we're all about helping you get through college with as little debt as possible. 
         We've gathered every financial aid option available in your school to make finding and applying for assistance easy and quick—no more waiting weeks for advisor appointments. 
         With FSAH, you're on track to land your dream job after graduation without the heavy burden of student loans!</div>
         <!-- Next button -->
         <button id="next-button" style="display: none;">Next</button>
         <div class="additional-info">
-        Do you need help with anything? Yes or No?
-        <div class="button-container">
-            <button id="yes-button">Yes</button>
-            <button id="no-button">No</button>
+            Do you need help with anything? Yes or No?
+            <div class="button-container">
+                <button id="yes-button">Yes</button>
+                <button id="no-button">No</button>
+            </div>
         </div>
 
-    </div>
-
-<!-- This will be hidden initially and only shown when "Yes" is clicked -->
-<div class="help-message" style="display: none;">
-    How can I help you?
-    <div class="help-buttons">
-        <button id="filter">Filter</button>
-        <button id="support">Support</button>
-        <button id="profile">Profile</button>
-        <button id="continue">Continue</button>
+        <!-- This will be hidden initially and only shown when "Yes" is clicked -->
+        <div class="help-message" style="display: none;">
+            How can I help you?
+            <div class="help-buttons">
+                <button id="filter">Filter</button>
+                <button id="support">Support</button>
+                <button id="profile">Profile</button>
+                <button id="continue">Continue</button>
+            </div>
+        </div>
     </div>
 </div>
 
-<!-- Filter Prompt -->
-<div id="filter-prompt" style="display: none;">
-    <h2>Filter Criteria</h2>
-    <form id="filter-form">
-        <!-- Filter by Type of Aid -->
-        <div>
-            <label for="type-of-aid">Type of Aid:</label>
-            <select id="type-of-aid">
-                <option value="grant">Grant</option>
-                <option value="scholarship">Scholarship</option>
-                <option value="loan">Loan</option>
-            </select>
-        </div>
-
-        <!-- Filter by Eligibility -->
-        <div>
-            <label for="eligibility">Eligibility:</label>
-            <select id="eligibility">
-                <option value="undergraduate">Undergraduate</option>
-                <option value="graduate">Graduate</option>
-                <option value="international">International Students</option>
-            </select>
-        </div>
-
-        <!-- Filter by Grant Amount -->
-        <div>
-            <label for="grant-amount">Grant Amount:</label>
-            <select id="grant-amount">
-                <option value="under5000">Under $5000</option>
-                <option value="5000-10000">$5000 - $10000</option>
-                <option value="above10000">Above $10000</option>
-            </select>
-        </div>
-
-        <!-- Submit button -->
-        <button type="submit">Apply Filters</button>
-    </form>
-    <button onclick="closeFilterPrompt()">Close</button>
-</div>
-
-
-    <script src="scripts/main.js"></script>
+<script src="scripts/main.js"></script>
 </body>
 </html>
--- 491-FSAH/scripts/main.js
+++ 491-FSAH/scripts/main.js
@@ -102,8 +102,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var helpMessage = document.querySelector('.help-message');
     var additionalInfo = document.querySelector('.additional-info');
     var websitePrompt = document.querySelector('.website-prompt');
-    var filterButton = document.getElementById('filter');
-    var filterPrompt = document.getElementById('filter-prompt');
 
     // Function to hide all robot elements
     function hideRobot() {
@@ -115,16 +113,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Function to hide the additional info
     function hideAdditionalInfo() {
         additionalInfo.style.display = 'none';
-    }
-
-    // Function to show filter prompt
-    function showFilterPrompt() {
-        filterPrompt.style.display = 'block';
-    }
-
-    // Function to close filter prompt
-    window.closeFilterPrompt = function() {
-        filterPrompt.style.display = 'none';
     }
 
     // Event listener for the Yes button
@@ -142,11 +130,7 @@ document.addEventListener('DOMContentLoaded', function() {
         additionalInfo.style.display = 'none';
     });
 
-    // Event listener for the Filter button
-    filterButton.addEventListener('click', function() {
-        showFilterPrompt();
-    });
-
+    //Event listener if the user decides to click the Yes button
     // Select the Support button
     var supportButton = document.getElementById('support');
 
@@ -155,6 +139,22 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = 'supportPage.html'; // Redirect to the support page
     });
 
+    // Select the Profile button
+    var profileButton = document.getElementById('profile');
+
+    // Event listener for the Profile button
+    profileButton.addEventListener('click', function() {
+        window.location.href = 'profile.php';
+    });
+
+    // Select the Filter button
+    var filterButton = document.getElementById('filter');
+
+    // Event listener for the Filter button
+    filterButton.addEventListener('click', function() {
+        window.location.href = 'page9.html';
+    });
+    
     // Select the Continue button
     var continueButton = document.getElementById('continue');
 
@@ -165,32 +165,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-    // Select the Support button
-    var supportButton = document.getElementById('profile');
-
-    // Event listener for the Support button
-    supportButton.addEventListener('click', function() {
-        window.location.href = 'profile.php'; // Redirect to the support page
-    });
-
 
 
 //Filter
-document.getElementById('apply-filter').addEventListener('click', function() {
-    var results = document.getElementById('results');
-    var grants = [
-        {id: 'calGrantA', name: 'Cal Grant A'},
-        {id: 'calGrantB', name: 'Cal Grant B'},
-        {id: 'pellGrant', name: 'Pell Grant'},
-        {id: 'fseog', name: 'FSEOG'},
-        {id: 'mcs', name: 'MCS'},
-        {id: 'sug', name: 'SUG'}
-    ];
-
-    var selectedGrants = grants.filter(grant => document.getElementById(grant.id).checked)
-                               .map(grant => `<p>${grant.name}</p>`)
-                               .join('');
-
-    results.innerHTML = selectedGrants || '<p>No grants selected</p>';
-});
-
